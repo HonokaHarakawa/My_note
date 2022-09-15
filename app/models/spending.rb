312aa52fd5672547a949d2aca19da677b1803196
HonokaHarakawa <wow.hono313@gmail.com>
2022-09-15 13:05:42 +0000

    ヘッダーレイアウト変更

--- app/models/spending.rb
+++ app/models/spending.rb
@@ -2,5 +2,4 @@ class Spending < ApplicationRecord
   belongs_to :user
   belongs_to :genre
   validates :spending_amount, numericality: { only_integer: true }
-  validates :genre, presence: true
 end
