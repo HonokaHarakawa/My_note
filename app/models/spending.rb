dac3c357e8f44aee2d8de7e0dc4f7c42ab380893
HonokaHarakawa <wow.hono313@gmail.com>
2022-09-26 13:55:19 +0000

    spendingテーブル削除

--- app/models/spending.rb
+++
@@ -1,5 +0,0 @@
-class Spending < ApplicationRecord
-  belongs_to :user
-  belongs_to :genre
-  validates :spending_amount, numericality: { only_integer: true }
-end
--- db/migrate/20220926131620_remove_date_from_spendings.rb
+++
@@ -1,5 +0,0 @@
-class RemoveDateFromSpendings < ActiveRecord::Migration[5.2]
-  def change
-    remove_column :spendings, :date, :date
-  end
-end
